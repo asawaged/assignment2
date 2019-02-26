3ed8e70da10ab19ed2d8bd2294cd56ef92e93488
Auday <asawaged@unomaha.edu>
2019-02-26 00:57:53 -0600

    test

--- mfscrm/wsgi.py
+++ mfscrm/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for mfscrm project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more informatdion on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
